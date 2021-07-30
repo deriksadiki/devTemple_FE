61055c28431e5865873163dd186ab456b94d83c7
deriksadiki <deriksadiki@gmail.com>
2021-07-30 22:51:50 +0200

    serach part 2

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Search.css";
-let names = [25, 23, 15, 20, 24];
+let names = [25, 23, 15, 20];
+let names2 = [25, 23];
+let btns = ["Copy Email", "Send Inmail"];
 
 function Search() {
   return (
@@ -12,19 +14,45 @@ function Search() {
             <div className="devList">
               {names.map((val, indx) => {
                 return (
-                  <div className="devBox">
-                    <p>developer</p>
+                  <div className="devBox" style={{ fontSize: 12 }} key={indx}>
+                    <img
+                      className="devImg"
+                      src={
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU"
+                      }
+                    />
+                    <div style={{ position: "absolute" }}>
+                      <p>Jon Doe</p>
+                      <p>
+                        3 years experience <span> Intermediate</span>
+                      </p>
+                      <div>
+                        <button className="selectedBtn">
+                          Nodejs <span style={{ opacity: 0.5 }}>&#x2715;</span>
+                        </button>
+                        <button className="selectedBtn">
+                          Nodejs <span style={{ opacity: 0.5 }}>&#x2715;</span>
+                        </button>
+                        <button className="selectedBtn">
+                          Nodejs <span style={{ opacity: 0.5 }}>&#x2715;</span>
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
             </div>
             <div className="vLine" />
             <div className="devInfo">
-              <p>Jon Doe</p>
+              <p style={{ color: "#2d39a6", fontSize: 25, fontWeight: "bold" }}>
+                Jon Doe
+              </p>
               <table>
                 <tr>
-                  <td>Stack</td>
-                  <td className="skills">Projects</td>
+                  <td className="tableHeader">Stack</td>
+                  <td className="skills" style={{ opacity: 0.5 }}>
+                    Projects
+                  </td>
                 </tr>
                 <tr>
                   {names.map((val, indx) => {
@@ -51,26 +79,50 @@ function Search() {
                           <td className="leftRow">
                             <div className="barBtn">Javascript</div>
                             <div className="matrixparent">
-                              <div
-                                className="matrixchild"
-                                style={{ width: parseInt("70") + "%" }}
-                              >
-                                <span className="matrixtext">60-80%</span>
-                              </div>
+                              <span className="matrixtext">60-80%</span>
                             </div>
                           </td>
                         </div>
                       );
-                    })}{" "}
+                    })}
+                  </td>
+                </tr>
+                <br></br>
+                <tr>
+                  <td className="tableHeader">Other Links</td>
+                  <td className="skills" style={{ opacity: 0.5 }}>
+                    Actions
                   </td>
                 </tr>
                 <tr>
-                  <td>Other Links</td>
-                  <td>Actions</td>
-                </tr>
-                <tr>
-                  <td>Stack</td>
-                  <td>Projects</td>
+                  <td>
+                    {names2.map((val, indx) => {
+                      return (
+                        <div>
+                          <td className="leftRow">
+                            <div className="barBtn">Javascript</div>
+                            <div className="matrixparent">
+                              <span className="matrixtext">60-80%</span>
+                            </div>
+                          </td>
+                        </div>
+                      );
+                    })}
+                  </td>
+                  <td className="skills">
+                    {btns.map((val, indx) => {
+                      return (
+                        <div>
+                          <td className="leftRow">
+                            <div className="barBtn2">{val}</div>
+                            <div className="matrixparent">
+                              <span className="matrixtext">60-80%</span>
+                            </div>
+                          </td>
+                        </div>
+                      );
+                    })}
+                  </td>
                 </tr>
               </table>
             </div>
